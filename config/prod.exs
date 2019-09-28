1714eb872d796d0c3bb61b35aeefd842a6ecfcb6
gheidorn@cars.com <greg.heidorn@gmail.com>
2019-09-27 23:30:09 -0500

    fixed broken prod cfg

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,7 @@ use Mix.Config
 config :keeper, KeeperWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [scheme: "https", host: "still-atoll-56227", port: 443],
-  force_ssl: [rewrite_on: [:x-forwarded_proto]]
+  force_ssl: [rewrite_on: [:x - forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
